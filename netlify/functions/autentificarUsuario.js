4077c2a549aec7191b7e8acfb3c356cff42b5b17
ming-yy <94019006+ming-yy@users.noreply.github.com>
2023-12-14 00:32:30 +0100

    Update autentificarUsuario.js

--- netlify/functions/autentificarUsuario.js
+++ netlify/functions/autentificarUsuario.js
@@ -1,13 +1,34 @@
 
-export default async (req, context) => {
-    //const requestKey = req.headers.get("X-API-Key");
-    console.log(req);
-    console.log(req.body);
-    console.log(req.body['email']);
-    const requestKey = req.body.get("email");
-    console.log(requestKey)
-    //const apiKey = Netlify.env.get("MY_API_KEY");
+const { MongoClient } = require("mongodb");
 
-    return new Response("Welcome!");
-  };
+const url = "mongodb+srv://admin:rsF2ABlIIwjSGuyZ@atlascluster.dfp8tyo.mongodb.net/?retryWrites=true&w=majority"
+
+const mongoClient = new MongoClient(url);
+const clientPromise = mongoClient.connect();
+
+
+export default async (req, context) => {
+   console.log(req);
+   console.log(req.body);
+
+   const contenido = req.json();
+   console.log(contenido['email']);
+   
+   email = contenido['email'];
+   contrasenya = contenido['password'];
+
+   try {
+      const database = (await clientPromise).db('appHorario');
+      const collection = database.collection('users');
+
+      const usuario = collection.find_one({"email": email.toString()});
+      if (usuario && usuario['password'] == contrasenya.toString()) {
+         return true;
+      }
+      return false;
+
+   } catch (error) {
+      return { statusCode: 500, body: error.toString() }
+   }
+};
   
